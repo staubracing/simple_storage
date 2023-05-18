d689562932f5c66ef6340599074af985cff16610
StaubRacing <staubracing@gmail.com>
2023-05-18 06:02:32 -0500

    Added functions to check you names and bike numbers

--- contracts/storage.sol
+++ contracts/storage.sol
@@ -2,6 +2,10 @@
 
 pragma solidity ^0.8.0;
 
+/// @title Storage contract
+/// @author Staub
+/// @notice A simple storage contract
+
 contract RacerRegistry {
     struct Rider {
         string name;
@@ -14,13 +18,21 @@ contract RacerRegistry {
     mapping(address => Rider) public riders;
 
     // Function to set the name of the rider
- function racerName(string memory _name, uint16 _bikeNumber, address _riderAddress) public {
+function racerName(string memory _name, uint16 _bikeNumber, address _riderAddress) public {
     require(!nameExists(_name), "Name already exists");
     require(!bikeNumberExists(_bikeNumber, _riderAddress), "Bike number already exists"); // Check if bike number already exists in the list of riders 
     ridersList.push(Rider(_name, _bikeNumber, _riderAddress)); // Add rider to the list
     riders[_riderAddress] = Rider(_name, _bikeNumber, _riderAddress); // Add rider to the mapping
 }
 
+    // Function to set the bike number of the rider
+function racerBikeNumber(uint16 _bikeNumber, address _riderAddress) public {
+    require(!bikeNumberExists(_bikeNumber, _riderAddress), "Bike number already exists"); // Check if bike number already exists in the list of riders 
+    ridersList.push(Rider(riders[_riderAddress].name, _bikeNumber, _riderAddress)); // Add rider to the list
+    riders[_riderAddress] = Rider(riders[_riderAddress].name, _bikeNumber, _riderAddress); // Add rider to the mapping
+}
+
+    // Function to check if the name already exists
 function nameExists(string memory _name) public view returns (bool) {
     for (uint256 i = 0; i < ridersList.length; i++) {
         if (keccak256(bytes(ridersList[i].name)) == keccak256(bytes(_name))) {
@@ -30,6 +42,7 @@ function nameExists(string memory _name) public view returns (bool) {
     return false;
 }
 
+    // Function to check if the bike number already exists
 function bikeNumberExists(uint16 _bikeNumber, address _riderAddress) public view returns (bool) {
     for (uint256 i = 0; i < ridersList.length; i++) {
         if (ridersList[i].bikeNumber == _bikeNumber && ridersList[i].riderAddress != _riderAddress) {
@@ -38,4 +51,34 @@ function bikeNumberExists(uint16 _bikeNumber, address _riderAddress) public view
     }
     return false;
 }
+
+    // Function to get the name of the rider
+function getRiderName(address _riderAddress) public view returns (string memory) {
+    return riders[_riderAddress].name;
+}
+
+    // Function to get the bike number of the rider
+function getRiderBikeNumber(address _riderAddress) public view returns (uint16) {
+    return riders[_riderAddress].bikeNumber;
+}
+
+    // Function to get a list of all riders by name
+function getRiderNames() public view returns (string[] memory) {
+    string[] memory names = new string[](ridersList.length);
+    for (uint256 i = 0; i < ridersList.length; i++) {
+        names[i] = ridersList[i].name;
+    }
+    return names;
+}
+
+    // Function to get the address of the rider
+function getRiderAddress(address _riderAddress) public view returns (address) {
+    return riders[_riderAddress].riderAddress;
+}
+
+    // Function to get the total number of riders
+function getRidersCount() public view returns (uint256) {
+    return ridersList.length;
+}
+
 }
