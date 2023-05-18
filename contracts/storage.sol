5c544774c949be83b2df0bf51e243e19add54d91
StaubRacing <staubracing@gmail.com>
2023-05-17 19:42:47 -0500

    First add

--- contracts/storage.sol
+++ contracts/storage.sol
@@ -5,16 +5,37 @@ pragma solidity ^0.8.0;
 contract RacerRegistry {
     struct Rider {
         string name;
-        uint256 bikeNumber;
+        uint16 bikeNumber;
+        address riderAddress;
     }
 
     Rider[] public ridersList;
 
-    // mapping(address => Rider) public riders;
+    mapping(address => Rider) public riders;
 
     // Function to set the name of the rider
-    function racerName(string memory _name) public {
-        ridersList.push(Rider(_name, 0)); // Add rider to the list
-        
+ function racerName(string memory _name, uint16 _bikeNumber, address _riderAddress) public {
+    require(!nameExists(_name), "Name already exists");
+    require(!bikeNumberExists(_bikeNumber, _riderAddress), "Bike number already exists"); // Check if bike number already exists in the list of riders 
+    ridersList.push(Rider(_name, _bikeNumber, _riderAddress)); // Add rider to the list
+    riders[_riderAddress] = Rider(_name, _bikeNumber, _riderAddress); // Add rider to the mapping
+}
+
+function nameExists(string memory _name) public view returns (bool) {
+    for (uint256 i = 0; i < ridersList.length; i++) {
+        if (keccak256(bytes(ridersList[i].name)) == keccak256(bytes(_name))) {
+            return true;
+        }
     }
+    return false;
+}
+
+function bikeNumberExists(uint16 _bikeNumber, address _riderAddress) public view returns (bool) {
+    for (uint256 i = 0; i < ridersList.length; i++) {
+        if (ridersList[i].bikeNumber == _bikeNumber && ridersList[i].riderAddress != _riderAddress) {
+            return true;
+        }
+    }
+    return false;
+}
 }
