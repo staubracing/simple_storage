d882330019a8cbe833e9e4c30078f1f822b72310
StaubRacing <staubracing@gmail.com>
2023-05-20 08:04:40 -0500

    Added Hardhat config file: require statments for hardhat, print list of accounts

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,19 @@
+
+const { task } = require('hardhat/config');
+
+require ("@nomicfoundation/hardhat-toolbox");
+require ("@nomicfoundation/hardhat-chai-matchers");
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 module.exports = {
   solidity: "0.8.18",
 };
--- test/test.js
+++ test/test.js
@@ -16,7 +16,12 @@ describe("RacerRegistry", function () {
         bikeNumber = 715;
         riderAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
     });
-
+    /// @notice Test that the contract is deployed
+    it("Should deploy the contract", async function () {
+        expect(await RacerRegistry.deployed()).to.equal(RacerRegistry);
+    }
+    );
+    
     it("Should register a racer", async function () {
         await RacerRegistry.racerName(name, bikeNumber, riderAddress);
         expect(await RacerRegistry.racerCount()).to.equal(1);
